19756483a7285b3b470bc01d97a3c3e959e3d149
Alex Drożdż <alexdrozdz@192.168.1.4>
2022-03-06 22:04:37 +0100

    best results ever

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import torchvision
 from torchvision.models.detection.rpn import AnchorGenerator
 from torchvision.models.detection.rpn import RegionProposalNetwork
 from torchvision.models.detection.rpn import RPNHead
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 from torch.utils.data import DataLoader
 from torchvision.models.detection import FasterRCNN
 from newspapersdataset import NewspapersDataset
@@ -24,16 +25,17 @@ warnings.filterwarnings("ignore")
 parameters = {
     'channel': 1, # 3 <= RGB, 1 <= greyscale
     'num_classes': 8, # 7 classes, but there is also one for background
-    'learning_rate': 5e-4,
+    'learning_rate': 25e-5,
     'batch_size': 16,
-    'num_epochs': 10,
-    'rescale': [375, 500], # if float, each image will be multiplied by it, if list [width, height] each image will be scaled to that size (concerns both images + annotations)
+    'num_epochs': 20,
+    'rescale': [600, 600], # if float, each image will be multiplied by it, if list [width, height] each image will be scaled to that size (concerns both images + annotations)
     'shuffle': False, 
     'weight_decay': 0, # regularization
-    'lr_step_size': 1, # lr scheduler step
-    'lr_gamma': 0.9, # lr step multiplier 
+    'lr_scheduler': True, # lr scheduler
+    'lr_step_size': 7, # lr scheduler step
+    'lr_gamma': .5, # lr step multiplier 
     'trainable_backbone_layers': 5, # 5 <= all, 0 <= any
-    'num_workers': 4,
+    'num_workers': 2,
     'main_dir': '/home/wmi/adrozdz/Master_degree/',
     'image_dir': '/home/wmi/adrozdz/scraped_photos_final/',
     'annotations_dir': '/home/wmi/adrozdz/Master_gonito/',
@@ -106,40 +108,55 @@ if parameters['train']:
         num_workers=parameters['num_workers'],
     )
 
-    # pre-trained model as a backbone
-    resnet = torchvision.models.detection.fasterrcnn_resnet50_fpn(
+##########################################
+    # pre-trained resnet50 model
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(
         pretrained=True,
-        trainable_backbone_layers=parameters['trainable_backbone_layers']  
-    )
-    backbone = resnet.backbone
-
-    # main model
-    model = FasterRCNN(
-        backbone,
-        num_classes=parameters['num_classes'],
+        trainable_backbone_layers=parameters['trainable_backbone_layers']
     )
 
-    # module that generates the anchors for a set of feature maps
-    anchor_generator = AnchorGenerator(
-        sizes=tuple([(16, 32, 64, 128, 256) for _ in range(5)]),
-        aspect_ratios=tuple([(0.75, 0.5, 1.25) for _ in range(5)])
-    )
+    # replace the pre-trained head with a new one
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    model.roi_heads.box_predictor = FastRCNNPredictor(
+        in_features, 
+        num_classes=parameters['num_classes']
+        )
+##########################################
 
-    # module that computes the objectness and regression deltas from the RPN
-    rpn_head = RPNHead(256, anchor_generator.num_anchors_per_location()[0])
+    # # pre-trained model as a backbone
+    # resnet = torchvision.models.detection.fasterrcnn_resnet50_fpn(
+    #     pretrained=True,
+    #     trainable_backbone_layers=parameters['trainable_backbone_layers']  
+    # )
+    # backbone = resnet.backbone
 
-    # region proposal network
-    model.rpn = RegionProposalNetwork(
-        anchor_generator=anchor_generator,
-        head=rpn_head,
-        fg_iou_thresh=0.7,
-        bg_iou_thresh=0.3,
-        batch_size_per_image=parameters['batch_size'],
-        positive_fraction=0.5,
-        pre_nms_top_n=dict(training=200, testing=100),
-        post_nms_top_n=dict(training=160, testing=80),
-        nms_thresh=0.7
-    )
+    # # main model
+    # model = FasterRCNN(
+    #     backbone,
+    #     num_classes=parameters['num_classes'],
+    # )
+
+    # # module that generates the anchors for a set of feature maps
+    # anchor_generator = AnchorGenerator(
+    #     sizes=tuple([(16, 32, 64, 128, 256) for _ in range(5)]),
+    #     aspect_ratios=tuple([(0.75, 0.5, 1.25) for _ in range(5)])
+    # )
+
+    # # module that computes the objectness and regression deltas from the RPN
+    # rpn_head = RPNHead(256, anchor_generator.num_anchors_per_location()[0])
+
+    # # region proposal network
+    # model.rpn = RegionProposalNetwork(
+    #     anchor_generator=anchor_generator,
+    #     head=rpn_head,
+    #     fg_iou_thresh=0.7,
+    #     bg_iou_thresh=0.3,
+    #     batch_size_per_image=parameters['batch_size'],
+    #     positive_fraction=0.5,
+    #     pre_nms_top_n=dict(training=200, testing=100),
+    #     post_nms_top_n=dict(training=160, testing=80),
+    #     nms_thresh=0.7
+    # )
 
     # optimizer
     optimizer = optim.Adam(
@@ -147,13 +164,15 @@ if parameters['train']:
         lr=parameters['learning_rate'],
         weight_decay=parameters['weight_decay']
     )
-
-    # learning rate scheduler decreases the learning rate by 10x every 3 epochs
-    lr_scheduler = torch.optim.lr_scheduler.StepLR(
-        optimizer,
-        step_size=parameters['lr_step_size'],
-        gamma=parameters['lr_gamma']
-    )
+    # learning rate scheduler decreases the learning rate by 'gamma' every 'step_size'
+    if parameters['lr_scheduler']:
+        lr_scheduler = torch.optim.lr_scheduler.StepLR(
+            optimizer,
+            step_size=parameters['lr_step_size'],
+            gamma=parameters['lr_gamma']
+        )
+    else:
+        lr_scheduler = None
 
     # training
     trained_model = train_model(

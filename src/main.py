9917f20483f701580dd82dbfaff91c08e9e83951
Alex Drożdż <alexdrozdz@192.168.1.13>
2022-03-15 01:59:09 +0100

    record breaking approach

--- src/main.py
+++ src/main.py
@@ -25,15 +25,15 @@ warnings.filterwarnings("ignore")
 parameters = {
     'channel': 1, # 3 <= RGB, 1 <= greyscale
     'num_classes': 8, # 7 classes, but there is also one for background
-    'learning_rate': 25e-5,
+    'learning_rate': 3e-4,
     'batch_size': 16,
     'num_epochs': 20,
-    'rescale': [600, 600], # if float, each image will be multiplied by it, if list [width, height] each image will be scaled to that size (concerns both images + annotations)
+    'rescale': [1000, 1000], # if float, each image will be multiplied by it, if list [width, height] each image will be scaled to that size (concerns both images + annotations)
     'shuffle': False, 
     'weight_decay': 0, # regularization
     'lr_scheduler': True, # lr scheduler
-    'lr_step_size': 7, # lr scheduler step
-    'lr_gamma': .5, # lr step multiplier 
+    'lr_step_size': 5, # lr scheduler step
+    'lr_gamma': .4, # lr step multiplier 
     'trainable_backbone_layers': 5, # 5 <= all, 0 <= any
     'num_workers': 2,
     'main_dir': '/home/wmi/adrozdz/Master_degree/',
